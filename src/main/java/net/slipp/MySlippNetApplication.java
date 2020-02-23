3149733fa62671afeefb777bfe0b970fb54f84aa
jhyoon6257@gmail.com <jhyoon6257@gmail.com>
2020-02-24 02:38:04 +0900

    initial commit

---
+++ src/main/java/net/slipp/MySlippNetApplication.java
@@ -0,0 +1,13 @@
+package net.slipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySlippNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySlippNetApplication.class, args);
+	}
+
+}
