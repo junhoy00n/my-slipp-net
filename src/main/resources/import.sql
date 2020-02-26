a892f80d7eb33eb2465fecd4b46255de42cd2d5c
jhyoon6257@gmail.com <jhyoon6257@gmail.com>
2020-02-26 21:44:21 +0900

    User와의 관계 매핑 및 생성일 추가 수정/삭제 기능 추가 및 LocalDateTime설정 답변 추가 및 목록 기능 구현 Exception을 통한 중복 코드 제거

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
-INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('test', 'pass', '테스트', 'test@gmail.com');
-INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('test2', 'pass', '테스트2', 'test2@gmail.com');
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (1, 'test', 'pass', 'test1', 'test@gmail.com');
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (2, 'test2', 'pass', 'test2', 'test2@gmail.com');
+
+INSERT INTO QUESTION (id, writer_id, title, contents, create_date) VALUES(1, 1, 'test1', 'tet1', CURRENT_TIMESTAMP());
+INSERT INTO QUESTION (id, writer_id, title, contents, create_date) VALUES(2, 2, 'test2', 'tet2', CURRENT_TIMESTAMP());
