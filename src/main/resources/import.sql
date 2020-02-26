02978382cd2fd3d1037b3073dc8f2ececb726965
jhyoon6257@gmail.com <jhyoon6257@gmail.com>
2020-02-27 05:38:05 +0900

    도메인 클래스 중복 제거 및 리펙토링

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (1, 'test', 'pass', 'test1', 'test@gmail.com');
-INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (2, 'test2', 'pass', 'test2', 'test2@gmail.com');
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL, CREATE_DATE) VALUES (1, 'test', 'pass', 'test1', 'test@gmail.com', CURRENT_TIMESTAMP());
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL, CREATE_DATE) VALUES (2, 'test2', 'pass', 'test2', 'test2@gmail.com', CURRENT_TIMESTAMP());
 
 INSERT INTO QUESTION (id, writer_id, title, contents, create_date, count_of_answer) VALUES(1, 1, 'test1', 'tet1', CURRENT_TIMESTAMP(), 0);
 INSERT INTO QUESTION (id, writer_id, title, contents, create_date, count_of_answer) VALUES(2, 2, 'test2', 'tet2', CURRENT_TIMESTAMP(), 0);
